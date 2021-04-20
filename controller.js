b3f7ebe73170d3f859fd9755b508538709e375a4
sejfudin <sejfudin.duranovic@gmail.com>
2021-04-21 01:50:13 +0200

    Creating Node server using Express and middlewares

--- controller.js
+++ controller.js
@@ -1,14 +1,11 @@
 const root = (req, res) => {
-    console.log(req.url);
     res.end('Home Page is comming!');
 }
 const text = (req, res) => {
-    console.log(req.url);
     const param = req.params.param || 'Text';
     res.end(`Hello ${param} Server!`);
 }
 const html = (req, res) => {
-    console.log(req.url);
     const param = req.params.param || 'Html';
     res.end(`<h1>Hello ${param} Server!</h1>`);
 }
--- router.js
+++ router.js
@@ -1,12 +1,13 @@
 const { root, text, html } = require('./controller');
+const { log, checkSize } = require('./middlewares');
 const router = app => {
-    app.get('/', root)
-    app.get('/text', text)
-    app.get('/text/:param', text)
+    app.get('/', log, root)
 
+    app.get('/text', log, text)
+    app.get('/text/:param', log, checkSize, text)
 
-    app.get('/html', html)
-    app.get('/html/:param', text)
+    app.get('/html', log, html)
+    app.get('/html/:param', log, checkSize, text)
 
 }
 module.exports = router;
